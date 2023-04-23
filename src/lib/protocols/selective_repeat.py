379cd74a338d324c362d10d00dca9eadebc9f658
fmonpelat <facundo.monpelat@gmail.com>
2023-04-23 13:13:59 -0300

    added changes to procol

--- src/lib/protocols/selective_repeat.py
+++ src/lib/protocols/selective_repeat.py
@@ -1,10 +1,8 @@
-
-
 import logging
 from lib.sockets_rdt.stream_rdt import StreamRDT
 
-
 class SlidingWindow:
+    
     def __init__(self, data, window_size, initial_seq_num=0):
         self.window_size = window_size
         self.data = data
@@ -18,6 +16,8 @@ class SlidingWindow:
         return self.ack_list[seq_num - self.current_seq_num]
 
     def set_ack(self, seq_num):
+        if(seq_num < self.current_seq_num or seq_num > self.final_seq_num):
+            return
         self.ack_list[seq_num - self.current_seq_num] = True
         self.update_sliding_window()
 
@@ -46,12 +46,8 @@ class SlidingWindow:
     def set_sent(self, seq_num, value: bool):
         self.sent_list[seq_num - self.current_seq_num] = value
 
-    # def get_current_window(self):
-    #     return self.data[:self.window_size]
-
-        # return {i + self.current_seq_num: self.data[i] for i in range(self.current_seq_num - self.initial_seq_num, min(self.current_seq_num - self.initial_seq_num + self.window_size, len(self.data)))}
-
-    def is_all_data_ack(self):
+    def finished(self):
+        # finished when all data is ack and current seq num is final seq num
         return self.current_seq_num == self.final_seq_num
 
     def is_available_segment_to_send(self, seq_num):
@@ -59,6 +55,7 @@ class SlidingWindow:
 
     def has_available_segments_to_send(self):
         i = self.current_seq_num
+        # is there any segment that is not sent and not ack in the current window?
         while i <= self.current_seq_num + self.window_size:
             if self.is_available_segment_to_send(i):
                 return True
@@ -76,62 +73,105 @@ class SlidingWindow:
             i += 1
         return None, None
 
-
 class SelectiveRepeat:
 
-    def __init__(self, stream: StreamRDT, mss: int):
+    def __init__(self, stream: StreamRDT, window_size, mss: int):
         self.stream = stream
         self.seq_num = self.stream.seq_num
         self.ack_num = self.stream.ack_num
-        self.window_size = 5
+        self.window_size = window_size
         self.mss = mss
 
-    def send(self, data):
-        # separating data into segments by mss
-        data_segments = self.get_segment_data(data)
+    def read(self):
+        # create data list
+        # while True:
+        # read segment from stream
+        # if segment has a consecutive seq num then send ack and concatenate data to data list
+        # if segment has consecutive seq num check if buffer has all data consecutively then send ack and concatenate all buffer data to data list
+        # if segment has a non consecutive seq num then send ack with last consecutive seq num and put data in buffer
+        # if segment has a fin flag then send ack and break
+        # return data list
+        buffer = {}
+        while True:
+            try:
+                # Reading segment
+                received_segment, external_addres = self.stream.read_segment()
+            except Exception as e:
+                continue
+            received_seq_num = received_segment.header.seq_num
+            received_ack_num = received_segment.header.ack_num
+            received_segment_data = received_segment.data
+            logging.info(f"Received segment {external_addres} seq_num: {received_seq_num} | data: {received_segment_data} | ack_num: {received_ack_num} | syn: False | fin: False")
+            
+            # send ack
+            logging.info(f"Sending Ack segment seq_num: {self.seq_num} | data: {b''} | ack_num: {received_seq_num} | syn: False | fin: False")
+            self.stream.send_segment(b'', self.seq_num, received_seq_num, False, False)
+
+            # if a a consecutive ack num was received
+            if self.ack_num + 1  == received_seq_num:
+                # update ack
+                self.ack_num += 1
+                return received_segment_data
+            else: # if not consecutive
+                logging.info(f"Non Consecutive segment seq_num: {received_seq_num} | data: {received_segment_data} | ack_num: {received_ack_num} | syn: False | fin: False")
+               
+                # add to buffer if it does not exist
+                if buffer.get(received_seq_num) == None:
+                    buffer[received_seq_num] = received_segment_data
+
+                # sort buffer by first key of tuple
+                sortedList = sorted(buffer.keys())
+                # check if sortedList values consecutive
+                if self.is_consecutive(sortedList):
+                    # if consecutive then concatenate all data to data list
+                    data = b''
+                    for key in sortedList:
+                        data += buffer[key]
+                    return data
+
+    def is_consecutive(self, list):
+        for i in range(len(list) - 1):
+            if list[i] + 1 != list[i+1]:
+                return False
+        return True
+
+    def send(self, data_segments):
         window = SlidingWindow(
             data_segments, self.window_size, self.seq_num)
 
-        # while there are still segments to send
-
         retries = 0
-        while not window.is_all_data_ack() and retries < self.MAX_TIMEOUT_RETRIES:
-
+        while not window.finished() and retries < self.MAX_TIMEOUT_RETRIES:
             if not window.has_available_segments_to_send():
-
                 try:
-                    # read blocking
-                    window.set_ack(received_seq_num)
+                    received_segment, _  = self.stream.read_segment()
+                    received_seq_num = received_segment.header.seq_num
+                    received_segment_data = received_segment.data
+                    received_ack_num = received_segment.header.ack_num
+                    logging.info(f"Received segment {external_addres} seq_num: {received_seq_num} | data: {received_segment_data} | ack_num: {received_ack_num} | syn: False | fin: False")
+                    window.set_ack(received_ack_num)
                     retries = 0
                     continue
                 except TimeoutError:
-                    # resend all segments
                     window.reset_sent_segments()
                     retries += 1
                     continue
+                except ValueError:
+                    continue
 
-            segment, last_sent_seq_num = window.get_first_available_segment()
 
-            # send_segment
-            window.set_sent(last_sent_seq_num, True)
+            segment, sent_seq_num = window.get_first_available_segment()
 
-            try:
-                # received_segment, received_seq_num =  read non blocking
-                window.set_ack(received_seq_num)
-                retries = 0
+            # send segment
+            logging.info(f"Sending segment seq_num: {sent_seq_num} | data: {segment} | ack_num: {self.ack_num} | syn: False | fin: False")
+            self.stream.send_segment(segment, sent_seq_num, self.ack_num, False, False)
+            window.set_sent(sent_seq_num, True)
+           
+            received_segment, external_addres  = self.stream.read_segment_non_blocking()
+            if received_seq_num == None:
                 continue
-            except TimeoutError:
-                continue
-
-    def get_segment_data(self, data):
-        # separating data into segments by mss
-        data_segments = []
-        for i in range(0, len(data), self.mss):
-            data_segments.append(data[i: i + self.mss])
-        return data_segments
-
-    def filter_segments(self, segments):
-        segments = []
-        for seq_num, segment in segments:
-            if seq_num in self.sent_segments:
-                del segments[seq_num]
+            received_seq_num = received_segment.header.seq_num
+            received_segment_data = received_segment.data
+            received_ack_num = received_segment.header.ack_num
+            logging.info(f"Received segment {external_addres} seq_num: {received_seq_num} | data: {received_segment_data} | ack_num: {received_ack_num} | syn: False | fin: False")
+            window.set_ack(received_ack_num)
+            retries = 0
